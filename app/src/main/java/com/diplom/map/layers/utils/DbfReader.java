9184e18d924df0c893073239b1e719f70485d171
DonGlebon <1g1s@mail.ru>
2019-03-19 01:13:39 +0300

    code refactoring,add kotlin migration for DbfStreamReader

--- app/src/main/java/com/diplom/map/layers/utils/DbfReader.java
+++ app/src/main/java/com/diplom/map/layers/utils/DbfReader.java
@@ -1,147 +1,69 @@
 package com.diplom.map.layers.utils;
 
-import com.bbn.openmap.dataAccess.shape.DbfTableModel;
-import com.bbn.openmap.dataAccess.shape.input.LittleEndianInputStream;
-
-import java.io.*;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CharsetEncoder;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class DbfReader {
-    /**
-     * An input stream to process primitives in Little Endian or Big Endian
-     */
-    private DbfStreamReader _leis = null;
 
-    /**
-     * An array of column names, as read from the field descripter array
-     */
+    private DbfStreamReader _leis;
+
     private String[] _columnNames = null;
 
-    /**
-     * An array of column lengths, as read from the field descripter array
-     */
     private byte[] _lengths = null;
 
-    /**
-     * An array of decimal counts, as read from the field descripter array
-     */
     private byte[] _decimalCounts = null;
 
-    /**
-     * An array of column types, as read from the field descripter array
-     */
     private byte[] _types = null;
 
-    /**
-     * The number of columns
-     */
     private int _columnCount = -1;
 
-    /**
-     * The number of rows
-     */
     private int _rowCount = -1;
 
-    /**
-     * The header length
-     */
     private short _headerLength = -1;
 
-    /**
-     * The record length
-     */
     private short _recordLength = -1;
 
-    /**
-     * An ArrayList with each element representing a record, which itself is an ArrayList
-     */
     private ArrayList _records = null;
 
-    /**
-     * Creates a LittleEndianInputStream then uses it to read the contents
-     * of the DBF file
-     *
-     * @param is An inputstream used to create a LittleEndianInputStream
-     */
-    public DbfReader(InputStream is) throws Exception {
-        BufferedInputStream bis = new BufferedInputStream(is);
-        // new InputStreamReader(new DataInputStream(is));
-        // _leis = new LittleEndianInputStream(is);
+    public DbfReader(InputStream stream) throws Exception {
+        BufferedInputStream bis = new BufferedInputStream(stream);
         _leis = new DbfStreamReader(bis);
         readHeader();
         readFieldDescriptors();
         readData();
     }
 
-    /**
-     * Returns an array of column names
-     *
-     * @return An array of column names
-     */
     public String[] getColumnNames() {
         return _columnNames;
     }
 
-    /**
-     * Returns an array of character lengths
-     *
-     * @return An array of character lengths
-     */
     public byte[] getLengths() {
         return _lengths;
     }
 
-    /**
-     * Returns an array of decimal counts
-     *
-     * @return An array of decimal counts
-     */
     public byte[] getDecimalCounts() {
         return _decimalCounts;
     }
 
-    /**
-     * Returns an array of field types
-     *
-     * @return An array of field types
-     */
     public byte[] getTypes() {
         return _types;
     }
 
-    /**
-     * Returns an ArrayList of records
-     *
-     * @return An ArrayList of recrods
-     */
     public ArrayList getRecords() {
         return _records;
     }
 
-    /**
-     * Returns the number of columns
-     *
-     * @return The nunber of columns
-     */
     public int getColumnCount() {
         return _columnCount;
     }
 
-    /**
-     * Returns the number of rows
-     *
-     * @return The number of rows
-     */
     public int getRowCount() {
         return _rowCount;
 
     }
 
-    /**
-     * Reads the header
-     */
     private void readHeader() throws IOException {
         byte description = _leis.readByte();
         byte year = _leis.readByte();
@@ -154,10 +76,6 @@ public class DbfReader {
         _leis.skipBytes(20);
     }
 
-    /**
-     * Initializes arrays that hold column names, column types, character
-     * lengths, and decimal counts, then populates them
-     */
     private void readFieldDescriptors() throws IOException {
         _columnNames = new String[_columnCount];
         _types = new byte[_columnCount];
@@ -174,9 +92,6 @@ public class DbfReader {
         }
     }
 
-    /**
-     * Reads the data and places data in a class scope ArrayList of records
-     */
     private void readData() throws IOException {
         _leis.skipBytes(2);
         _records = new ArrayList();
@@ -188,9 +103,6 @@ public class DbfReader {
                 String cell = _leis.readString(length);
                 if (cell.contains("*")) {
                     record.add(c, "");
-                } else if (type == DbfTableModel.TYPE_NUMERIC && !cell.equals("")) {
-                    double d = new Double(cell);
-                    record.add(c, cell);
                 } else {
                     record.add(c, cell);
                 }
---
+++ app/src/main/java/com/diplom/map/layers/utils/DbfStreamReader.kt
@@ -0,0 +1,52 @@
+package com.diplom.map.layers.utils
+
+import java.io.DataInputStream
+import java.io.EOFException
+import java.io.IOException
+import java.io.InputStream
+import java.nio.charset.Charset
+
+internal class DbfStreamReader(stream: InputStream) : DataInputStream(stream) {
+
+    private val stream: DataInputStream = DataInputStream(stream)
+
+    @Throws(IOException::class)
+    fun readString(length: Int): String {
+        val array = ByteArray(length)
+        this.readFully(array)
+        val s = String(array, Charset.forName("windows-1251"))
+        return s.trim { it <= ' ' }
+    }
+
+    @Throws(IOException::class)
+    fun readLEShort(): Short {
+        val byte1 = this.stream.read()
+        val byte2 = this.stream.read()
+        return if (byte2 == -1) {
+            throw EOFException()
+        } else {
+            ((byte2 shl 8) + byte1).toShort()
+        }
+    }
+
+    @Throws(IOException::class)
+    fun readLEInt(): Int {
+        val byte1: Int
+        val byte2: Int
+        val byte3: Int
+        val byte4: Int
+        synchronized(this) {
+            byte1 = this.stream.read()
+            byte2 = this.stream.read()
+            byte3 = this.stream.read()
+            byte4 = this.stream.read()
+        }
+
+        return if (byte4 == -1) {
+            throw EOFException()
+        } else {
+            (byte4 shl 24) + (byte3 shl 16) + (byte2 shl 8) + byte1
+        }
+    }
+
+}
